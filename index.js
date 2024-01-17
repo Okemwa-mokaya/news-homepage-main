2493fc98673ef76b2afdb9305a575e83e7000792
Okemwa-mokaya <133957319+Okemwa-mokaya@users.noreply.github.com>
2024-01-17 15:42:56 +0300

    Subtractions

--- index.js
+++ index.js
@@ -7,11 +7,9 @@ const dropDownMenu = document.querySelector(".dropdown-menu");
 const closeBtn = document.querySelector(".close");
 
 toggleBtn.addEventListener("click", ()=>{
-    /*contentEl.classList.add("active")*/
     dropDownMenu.classList.add("open")
 });
     
 closeBtn.addEventListener("click", ()=>{
-    /*contentEl.classList.remove("active")*/
     dropDownMenu.classList.remove("open")
 });
